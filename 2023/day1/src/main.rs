b5f754e9b69b10e945c38648a4f9ea50d83b54a3
sahil <404salad@gmail.com>
2023-12-01 17:24:56 +0530

    it compiles ToT

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,7 +1,27 @@
 use util::get_lines;
+fn getsum(word: &str) -> u32 {
+    println!("got input {}", word);
+    if word.is_empty() {return 0};
+    let mut i = 0;
+    let mut j = word.len()-1;
+    for a in word.chars() {
+        if a.is_digit(10) {break;}
+        i+=1;
+    }
+    for a in word.chars().rev() {
+        if a.is_digit(10) {break;}
+        j-=1;
+    }
+    let mut ret = String::from("");
+    ret.push(word.chars().nth(i).unwrap());
+    ret.push(word.chars().nth(j).unwrap());
+    ret.parse::<u32>().unwrap()
+    
+}
 fn main() {
-    println!("Current working directory: {:?}", std::env::current_dir());
     get_lines(|lines| {
-        println!("{:?}",lines);
+        for word in lines {
+            println!("{} {}", word, getsum(word));
+        }
     })
 }
