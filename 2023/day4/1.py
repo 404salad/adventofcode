89c179a550d59ead71049cf49e8bdf4ce9a6970e
404salad <404salad@gmail.com>
2023-12-14 18:41:48 +0530

    day4

--- 2023/day4/1.py
+++ 2023/day4/1.py
@@ -1,9 +1,9 @@
-with open("demo.txt","r") as file:
+with open("input.txt","r") as file:
     ans = 0
     for line in file.readlines():
         stuff = line[:-1].split()
-        stuff1=list(map(int,stuff[2:7:]))
-        stuff2=list(map(int,stuff[8:]))
+        stuff1=list(map(int,stuff[2:12:]))
+        stuff2=list(map(int,stuff[13:]))
         mul = 1/2
         for el in stuff2:
             if el in stuff1:
--- 2023/day4/2.py
+++ 2023/day4/2.py
@@ -1,15 +1,21 @@
 with open("input.txt","r") as file:
-    ans = 0
-    for line in file.readlines():
+    lawn = 204 
+    ans = []
+    for i in range(lawn):
+        ans.append(1) 
+    for i,line in enumerate(file.readlines()):
         stuff = line[:-1].split()
         stuff1=list(map(int,stuff[2:12:]))
         stuff2=list(map(int,stuff[13:]))
-        mul = 1/2
+        count=0
         for el in stuff2:
             if el in stuff1:
-                mul*=2
-        if(mul!=0.5):
-            ans+=mul
-        print(mul)
-    print(int(ans))
+               count+=1 
+        print(i,ans[i],count)
+        for z in range(count):
+            if(i+z<lawn):
+                ans[i+z]+=ans[i-1]
+                
+    print(list(map(int,ans)))
+    print(sum(list(map(int,ans))))
 
